27f5cfdf7bc71d52291769e18e05c0802becadbf
Prachi-ranjan18 <prachi0198@gmail.com>
2025-07-11 20:33:03 +0530

    Initial commit: snappyShop recommender project structure

---
+++ user-activity-service/src/test/java/com/snappyshop/recommender/useractivity/UserActivityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snappyshop.recommender.useractivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserActivityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
